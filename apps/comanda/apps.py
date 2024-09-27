aa6c5618d5d05a1701fb5e0f1053841e54d17744
Ceci <78119165+Ceci-123@users.noreply.github.com>
2024-09-27 20:32:47 -0300

    armo las apps

---
+++ apps/comanda/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ComandaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'comanda'
---
+++ apps/pedidoTotal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PedidototalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pedidoTotal'
