8a4e49568d62ddf61ef9b006b60efe4ec52ce3fd
FacBar <facundobartolom3@gmail.com>
2024-09-27 21:06:18 -0300

    TablaEstado

--- apps/estado/models.py
+++ apps/estado/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Estado (models.Model):
+    nombre = models.CharField(max_lenght=25)
+
+    def __str__(self):
+        return self.nombre
