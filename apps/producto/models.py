0abd92e99d36c8ca9321af83f0be37ec59ec9f78
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-01 19:22:03 -0300

    feat: include product model

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Producto (models.Model):
+	precio = models.FloatField()
+	nombre = models.CharField(max_length=50)
+	tipo = models.ForeignKey("TipoProducto", on_delete=models.CASCADE)
+	
+class TipoProducto (models.Model):
+	nombre = models.CharField(max_length=50)
