9f6b2ef69c99d9df02b84e089c405119f7441745
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-01 19:31:52 -0300

    feat: create model factura

--- apps/factura/models.py
+++ apps/factura/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Factura(models.Model):
+	pago = models.FloatField()
+	id_ticket = models.ForeignKey("ticket.Ticket", on_delete=models.CASCADE)
