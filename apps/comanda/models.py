bbd474d759228c28bd5c7e027ef6a1616250daa6
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-01 19:30:48 -0300

    fix: use already model created Estado for model Comanda

--- apps/comanda/models.py
+++ apps/comanda/models.py
@@ -4,7 +4,5 @@ from django.db import models
 class Comanda (models.Model):
 		id_producto = models.ForeignKey("producto", on_delete=models.CASCADE)
 		cantidad = models.IntegerField()
-		id_estado = models.ForeignKey("Estado", on_delete=models.CASCADE)
+		id_estado = models.ForeignKey("estado.Estado", on_delete=models.CASCADE)
 		
-class Estado (models.Model):
-		nombre = models.CharField(max_length=50)
