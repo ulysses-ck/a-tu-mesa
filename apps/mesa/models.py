6a243ad51cb72080003201b00cf56c9eb5aca231
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-10 00:57:05 -0300

    feat: add field to mesa model
    add nro_mesa field missing in mesa model

--- apps/mesa/models.py
+++ apps/mesa/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Mesa (models.Model):
     id_comanda = models.ForeignKey("comanda", on_delete=models.CASCADE)
+    nro_mesa = models.IntegerField()
