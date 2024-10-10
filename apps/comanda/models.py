e373acadb3672123474a3bedd8bd00b5e5d56471
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-10 00:56:36 -0300

    Update Comanda model in apps/comanda/models.py
    - Modified the Comanda model structure
    - Adjusted relationships and fields as needed
    - Improved code organization and readability

--- apps/comanda/models.py
+++ apps/comanda/models.py
@@ -5,4 +5,6 @@ class Comanda (models.Model):
 		id_producto = models.ForeignKey("producto", on_delete=models.CASCADE)
 		cantidad = models.IntegerField()
 		id_estado = models.ForeignKey("estado.Estado", on_delete=models.CASCADE)
-		
+		id_tipo_de_pedido = models.ForeignKey("tipoDePedido.TipoDePedido", on_delete=models.CASCADE)
+		id_ticket = models.ForeignKey("ticket.Ticket", on_delete=models.CASCADE)
+		fecha = models.DateTimeField()
