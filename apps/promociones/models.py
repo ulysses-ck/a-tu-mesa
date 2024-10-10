798ca4e3add1debb42e19331e44c326d01c7847f
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-10 00:58:53 -0300

    feat: add models promociones
    add missing fields in promociones models and create condiciones model needed for it

--- apps/promociones/models.py
+++ apps/promociones/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Promociones (models.Model):
 		descuento = models.FloatField()
 		descripcion = models.CharField(max_length=50)
+		id_condiciones = models.ForeignKey("Condiciones", on_delete=models.CASCADE)
+		
+class Condiciones (models.Model):
+		inicio = models.DateTimeField()
+		expira = models.DateTimeField()
+		id_producto = models.ForeignKey("producto.Producto", on_delete=models.CASCADE)
