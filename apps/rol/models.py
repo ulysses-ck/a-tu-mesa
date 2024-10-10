211cac77ec0c8959e9db32a8ab4874e3569d783a
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-10 00:59:57 -0300

    feat: add models rol

--- apps/rol/models.py
+++ apps/rol/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Rol (models.Model):
+    nombre = models.CharField(max_lenght=25)
+
+    def __str__(self):
+        return self.nombre
