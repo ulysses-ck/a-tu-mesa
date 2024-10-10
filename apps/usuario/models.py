ab8f899d024b580ee54acfde4cd234d5221eb2e1
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-10 01:00:43 -0300

    feat: add model usuario

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Usuario (models.Model):
+    persona = models.ForeignKey("persona.Persona", on_delete=models.CASCADE)
+    contraseña = models.CharField(max_lenght=25)
+    
