b412f81b94400d6179e1a82007d2148b67b48912
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-10 00:58:23 -0300

    feat: add model pago and formadepago
    create pago and formadepago models with the fields needed

--- apps/pago/models.py
+++ apps/pago/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Pago (models.Model):
+	nro_autorizacion = models.IntegerField()
+	fecha = models.DateTimeField()
+	pagador = models.ForeignKey("persona.Persona", on_delete=models.CASCADE)
+	id_forma_de_pago = models.ForeignKey("FormaDePago", on_delete=models.CASCADE)
+
+class FormaDePago (models.Model):
+	nombre = models.CharField(max_lenght=25)
+
+	def __str__(self):
+		return self.nombre
