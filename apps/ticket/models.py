d914d8a8d3fa14cc73cce9c1011dacd5ad5d4fe8
FacBar <facundobartolom3@gmail.com>
2024-10-11 20:42:19 -0300

    Actualizar cardinalidades: -OnetoOne en user -ManyToMany en ticket

--- apps/ticket/models.py
+++ apps/ticket/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Ticket (models.Model):
-	id_promocion = models.ForeignKey("promociones.Promociones", on_delete=models.CASCADE)
+	id_promocion = models.ManyToManyField("promociones.Promociones")
 	fecha = models.DateTimeField()
 	notas = models.CharField(max_length=120)
--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Usuario (models.Model):
-    persona = models.ForeignKey("persona.Persona", on_delete=models.CASCADE)
+    persona = models.OneToOneField("persona.Persona", on_delete=models.CASCADE)
     contraseña = models.CharField(max_length=25)
 
