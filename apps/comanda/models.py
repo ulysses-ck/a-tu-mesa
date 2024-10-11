dff329be3c1f17e1e20d34f090b9f9dad54b2f3e
SIA (Soluciones Informáticas Amezaga) <diegoamezaga@gmail.com>
2024-10-10 21:38:05 -0300

    creo entidad pago

--- apps/comanda/models.py
+++ apps/comanda/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Comanda (models.Model):
-		id_producto = models.ForeignKey("producto", on_delete=models.CASCADE)
+		id_producto = models.ForeignKey("producto.Producto", on_delete=models.CASCADE)
 		cantidad = models.IntegerField()
 		id_estado = models.ForeignKey("estado.Estado", on_delete=models.CASCADE)
 		id_tipo_de_pedido = models.ForeignKey("tipoDePedido.TipoDePedido", on_delete=models.CASCADE)
--- apps/factura/models.py
+++ apps/factura/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Factura(models.Model):
-	pago = models.FloatField()
+	pago = models.ForeignKey("pago.Pago", on_delete=models.CASCADE)
 	id_ticket = models.ForeignKey("ticket.Ticket", on_delete=models.CASCADE)
