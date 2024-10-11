8dea433a4b28495c1313aca7beed3c92b31903a0
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-11 10:06:09 -0300

    feat: example class view

--- atumesa/urls.py
+++ atumesa/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.persona.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
 ]
