f4a616d4db62be08d761225341c1768737527506
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-11 19:29:13 -0300

    fix: import of class view

--- atumesa/urls.py
+++ atumesa/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.persona.views import HomeView
+from apps.producto.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
