ab17c9d749c109f95ec4cd4e4c4b59c3e8000fe9
SIA (Soluciones Informáticas Amezaga) <diegoamezaga@gmail.com>
2024-10-10 22:47:41 -0300

    migraciones exitosas

--- apps/tipoProducto/apps.py
+++ apps/tipoProducto/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TipoproductoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tipoProducto'
+    name = 'apps.tipoProducto'
