f147be0dec1d63d29a1c7166954a9f5d181f59fd
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-10 21:50:48 -0300

    refactor: django and local apps separate
    this to address to good practices

--- atumesa/settings.py
+++ atumesa/settings.py
@@ -30,13 +30,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+LOCAL_APPS = [
     'caja',
     'comanda',
     'estado',
@@ -50,6 +53,8 @@ INSTALLED_APPS = [
     'ticket',
     'tipoDePedido',
 ]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
