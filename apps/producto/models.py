5462c8bf6d0a3a9639cdd4b94558935af0174a14
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-11 21:44:02 -0300

    fix: models and admin
    show correctly its name attribute of the model at showing in admin view

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -6,5 +6,8 @@ class Producto (models.Model):
 	nombre = models.CharField(max_length=50)
 	tipo = models.ForeignKey("tipoProducto.TipoProducto", on_delete=models.CASCADE)
 	
+	def __str__(self):
+		return self.nombre
+
 class TipoProducto (models.Model):
 	nombre = models.CharField(max_length=50)
--- apps/tipoProducto/models.py
+++ apps/tipoProducto/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class TipoProducto(models.Model):
     nombre = models.CharField(max_length=25)
     
-    def _str_(self):
+    def __str__(self):
         return self.nombre
