1c9fd62061bc1ef10465cbb17dd5348062a1026f
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-11 21:10:15 -0300

    feat: include productos and variable
    include in the context all productos and home

--- apps/producto/views.py
+++ apps/producto/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-
-# Create your views here.
+from .models import Producto
 
 class HomeView(TemplateView):
+	name = 'home'
+	
+	# get all productos from database
+	productos = Producto.objects.all()
 	template_name = 'index.html'
+	
+	# render name in template
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['name'] = self.name
+		context['productos'] = self.productos
+		return context
