d6949aaf6fbe9c5ce8db92da651c70413f8438ea
ulysses-ck <ulisesapaza.280@gmail.com>
2024-10-29 21:23:11 -0300

    fix: add to admin page

--- apps/pago/admin.py
+++ apps/pago/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.pago.models import Pago
+from apps.pago.models import Pago, FormaDePago
 
 # Register your models here.
 
@@ -7,3 +7,8 @@ from apps.pago.models import Pago
 @admin.register(Pago)
 class PagoAdmin(admin.ModelAdmin):
     list_display = ('nro_autorizacion', 'fecha', 'pagador', 'forma_de_pago')
+
+@admin.register(FormaDePago)
+class FormaDePagoAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
