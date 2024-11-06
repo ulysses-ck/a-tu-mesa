e36df292a3c4df40cfb477948bcacf02041cf8eb
Ulises <ulisesapaza.280@mail.com>
2024-11-05 23:33:35 -0300

    fix: change settings to include views from home

--- atumesa/urls.py
+++ atumesa/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from apps.home.views import HomeView
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
-]
+    path('', include('apps.home.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
