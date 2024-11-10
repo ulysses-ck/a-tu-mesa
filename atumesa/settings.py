49c24874f4ef68858db299fa56d0216166e0b284
Ulises <ulisesapaza.280@gmail.com>
2024-11-10 20:01:44 -0300

    feat: add base html
    this using template html

--- atumesa/settings.py
+++ atumesa/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'atumesa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
