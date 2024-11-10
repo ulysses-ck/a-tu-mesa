0c54d0cbfedef54341e8a4f374e2a74051f34826
Ulises <ulisesapaza.280@gmail.com>
2024-11-10 18:17:06 -0300

    feat: add image field to producto

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -5,6 +5,7 @@ class Producto (models.Model):
 	precio = models.FloatField()
 	nombre = models.CharField(max_length=50)
 	tipo = models.ForeignKey("tipoProducto.TipoProducto", on_delete=models.CASCADE)
+	imagen = models.ImageField(upload_to='productos', null=True, blank=True)
 	
 	def __str__(self):
 		return self.nombre
