0bb56802c3e65b17b2bec763efae34da3e7f1bac
SIA (Soluciones Informáticas Amezaga) <diegoamezaga@gmail.com>
2024-11-09 22:51:50 -0300

    cambios en index, se agrega vista about, se prepara terreno para vista login

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.HomeAbout.as_view(), name='about'),
     path('edit/', views.HomeEditView.as_view(), name='edit_home'),
 ] 
--- apps/home/views.py
+++ apps/home/views.py
@@ -42,6 +42,8 @@ class HomeEditView(TemplateView):
             context['botonmenu'] = home.botonMenu
             context['botonreservacion'] = home.botonReservation
             context['botonseguimientodepedido'] = home.botonSeguimientoDePedido
+            self.template_name = f'{kwargs.get("template_name")}.html'
+
         return context
 
     def post(self, request, *args, **kwargs):
@@ -59,3 +61,25 @@ class HomeEditView(TemplateView):
         messages.success(request, 'Contenido actualizado exitosamente')
         return self.render_to_response(self.get_context_data(**kwargs))
 
+class HomeAbout(TemplateView):
+	name = 'about'
+	template_name = 'about.html'
+
+	# render name in template
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		home_instance, created = Home.objects.get_or_create(
+            id= 1,
+            defaults={
+                "banner": "https://i.ibb.co/wWdxVVs/ilustracion-delicioso-shoyu-ramen-vista-arriba-fondo-blanco-435599-30529.jpg",
+                "botonMenu":"https://cdn.pixabay.com/photo/2017/11/07/07/06/menu-2925825_1280.png",
+                "botonReservation":"https://w7.pngwing.com/pngs/629/948/png-transparent-red-round-book-now-button-book-now-buttons-thumbnail.png",
+                "botonSeguimientoDePedido":"https://as2.ftcdn.net/v2/jpg/00/16/27/39/1000_F_16273947_3TlCeyAVBlJuphSPEDmllmLUeyAAQezK.jpg"
+            }
+        )
+		if home_instance:
+			context['banner'] = home_instance.banner
+			context['botonmenu'] = home_instance.botonMenu
+			context['botonreservacion'] = home_instance.botonReservation
+			context['botonseguimientodepedido'] = home_instance.botonSeguimientoDePedido
+		return context
