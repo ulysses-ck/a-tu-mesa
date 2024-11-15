e3f74cf613753ddf65bf60ccf49ca8ca222b1324
Ceci <78119165+Ceci-123@users.noreply.github.com>
2024-11-15 20:44:35 -0300

    promociones

--- apps/promociones/views.py
+++ apps/promociones/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Promociones
+from django.contrib import messages
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.utils.decorators import method_decorator
+
+class PromocionesView(TemplateView):
+    name = 'promociones'
+    template_name = 'promociones.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['promociones'] = Promociones.objects.all()
+        return context
 
 # Create your views here.
