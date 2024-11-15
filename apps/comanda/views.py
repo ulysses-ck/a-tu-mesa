ad0596b9edb6b24ee25dee2493427eb32467aaae
Ceci <78119165+Ceci-123@users.noreply.github.com>
2024-11-15 20:48:40 -0300

    comanda

--- apps/comanda/views.py
+++ apps/comanda/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Comanda
+from django.contrib import messages
+from django.views.decorators.clickjacking import xframe_options_exempt
+from django.utils.decorators import method_decorator
 
+class ComandaView(TemplateView):
+    name = 'comanda'
+    template_name = 'comanda.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comanda'] = Comanda.objects.all()
+        return context
 # Create your views here.
