6ee491ff9b3f52eb0a5cc51af3f003dc866d1d08
Ulises <ulisesapaza.280@gmail.com>
2024-11-16 20:26:28 -0300

    fix: change resource from plural to single

--- apps/factura/urls.py
+++ apps/factura/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('facturas', views.FacturaView.as_view(), name='facturas'),
+    path('factura', views.FacturaView.as_view(), name='factura'),
     path('factura/create/', views.CreateFacturaView.as_view(), name='create_factura'),
     path('factura/update/<int:pk>/', views.UpdateFacturaView.as_view(), name='update_factura'),
     path('factura/delete/<int:pk>/', views.DeleteFacturaView.as_view(), name='delete_factura'),
