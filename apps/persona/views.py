68b88bc53176739b171b6e0c475540d063631d13
FacBar <facundobartolom3@gmail.com>
2024-11-15 21:42:20 -0300

    persona urls

--- apps/persona/views.py
+++ apps/persona/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.utils.decorators import method_decorator
 
-class ComandaView(TemplateView):
+class PersonaView(TemplateView):
     name = 'persona'
     template_name = 'persona.html'
     
