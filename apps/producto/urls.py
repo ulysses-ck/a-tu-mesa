d14f0d4938dad0ecbe6aeaf72be3a7358065db93
Ulises <ulisesapaza.280@gmail.com>
2024-11-16 20:21:16 -0300

    feat: create producto crud view

--- apps/producto/urls.py
+++ apps/producto/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('producto', views.ProductoView.as_view(), name='producto'),
+    # esto es para el menu vista cliente
+    path('menu', views.ProductoView.as_view(), name='producto'),
+
+    # admin
+    path('producto/', views.ProductoAdminView.as_view(), name='producto_admin'),
+    path('producto/create/', views.CreateProductoView.as_view(), name='create_producto'),
+    path('producto/update/<int:pk>/', views.UpdateProductoView.as_view(), name='update_producto'),
+    path('producto/delete/<int:pk>/', views.DeleteProductoView.as_view(), name='delete_producto'),
 ]
--- apps/producto/views.py
+++ apps/producto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from .models import Producto
 from django.contrib import messages
 from django.views.decorators.clickjacking import xframe_options_exempt
@@ -7,9 +7,45 @@ from django.utils.decorators import method_decorator
 
 class ProductoView(TemplateView):
     name = 'producto'
-    template_name = 'producto.html'
+    template_name = 'menu.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['productos'] = Producto.objects.all()
         return context
+    
+class ProductoAdminView(TemplateView):
+    name = 'producto_admin'
+    template_name = 'producto_read.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['productos'] = Producto.objects.all()
+        return context
+
+class CreateProductoView(CreateView):
+    model = Producto
+    fields = [
+        'precio',
+        'nombre',
+        'tipo',
+        'imagen',
+    ]
+    template_name = 'producto_create.html'
+    success_url = '/productos'
+
+class UpdateProductoView(UpdateView):
+    model = Producto
+    fields = [
+        'precio',
+        'nombre',
+        'tipo',
+        'imagen',
+    ]
+    template_name = 'producto_update.html'
+    success_url = '/productos'
+
+class DeleteProductoView(DeleteView):
+    model = Producto
+    template_name = 'producto_delete.html'
+    success_url = '/productos'
