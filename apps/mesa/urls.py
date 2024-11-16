6f6d4b5de13209fd25590c52f14c70811f8ede98
FacBar <facundobartolom3@gmail.com>
2024-11-16 18:30:24 -0300

    vistas y crud de mesa

--- apps/mesa/urls.py
+++ apps/mesa/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('mesa', views.MesaView.as_view(), name='mesa'),
+    path('mesa/create/', views.CreateMesaView.as_view(), name='create_mesa'),
+    path('mesa/update/<int:pk>/', views.UpdateMesaView.as_view(), name='update_mesa'),
+    path('mesa/delete/<int:pk>/', views.DeleteMesaView.as_view(), name='delete_mesa'),
 ]
--- apps/mesa/views.py
+++ apps/mesa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from .models import Mesa
 from django.contrib import messages
 from django.views.decorators.clickjacking import xframe_options_exempt
@@ -11,5 +11,26 @@ class MesaView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['mesa'] = Mesa.objects.all()
+        context['mesas'] = Mesa.objects.all()
         return context
+
+class CreateMesaView(CreateView):
+    model = Mesa
+    fields = [
+        'nro_mesa',
+    ]
+    template_name = 'mesa_create.html'
+    success_url = '/mesa'
+
+class UpdateMesaView(UpdateView):
+    model = Mesa
+    fields = [
+        'nro_mesa',
+    ]
+    template_name = 'mesa_update.html'
+    success_url = '/mesa'
+
+class DeleteMesaView(DeleteView):
+    model = Mesa
+    template_name = 'mesa_delete.html'
+    success_url = '/mesa'   
