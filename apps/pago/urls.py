f2cf34912effe4d639e5430dd96f676f2dfae544
FacBar <facundobartolom3@gmail.com>
2024-11-16 18:59:41 -0300

    crud de pago

--- apps/pago/urls.py
+++ apps/pago/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('pago', views.PagoView.as_view(), name='pago'),
+    path('pago/create/', views.CreatePagoView.as_view(), name='create_pago'),
+    path('pago/update/<int:pk>/', views.UpdatePagoView.as_view(), name='update_pago'),
+    path('pago/delete/<int:pk>/', views.DeletePagoView.as_view(), name='delete_pago'),
 ]
--- apps/pago/views.py
+++ apps/pago/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from .models import Pago
 from django.contrib import messages
 from django.views.decorators.clickjacking import xframe_options_exempt
@@ -7,9 +7,35 @@ from django.utils.decorators import method_decorator
 
 class PagoView(TemplateView):
     name = 'Pago'
-    template_name = 'cPago.html'
+    template_name = 'pago.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['Pago'] = Pago.objects.all()
+        context['pagos'] = Pago.objects.all()
         return context
+class CreatePagoView(CreateView):
+    model = Pago
+    fields = [
+        'nro_autorizacion',
+        'fecha',
+        'pagador',
+        'forma_de_pago',
+    ]
+    template_name = 'pago_create.html'
+    success_url = '/pago'
+
+class UpdatePagoView(UpdateView):
+    model = Pago
+    fields = [
+        'nro_autorizacion',
+        'fecha',
+        'pagador',
+        'forma_de_pago',
+    ]
+    template_name = 'pago_update.html'
+    success_url = '/pago'
+
+class DeletePagoView(DeleteView):
+    model = Pago
+    template_name = 'pago_delete.html'
+    success_url = '/pago'   
