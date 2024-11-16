e3f0588c6af8e4d9a29f6a1a11049e16d7fcfe6a
FacBar <facundobartolom3@gmail.com>
2024-11-16 17:48:44 -0300

    vistas y crud de factura

--- apps/factura/urls.py
+++ apps/factura/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('facturas', views.FacturaView.as_view(), name='facturas'),
+    path('factura/create/', views.CreateFacturaView.as_view(), name='create_factura'),
+    path('factura/update/<int:pk>/', views.UpdateFacturaView.as_view(), name='update_factura'),
+    path('factura/delete/<int:pk>/', views.DeleteFacturaView.as_view(), name='delete_factura'),
 ]
--- apps/factura/views.py
+++ apps/factura/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
+from .models import Factura
 # Create your views here.
 class FacturaView(TemplateView):
     name = 'facturas'
@@ -7,4 +7,28 @@ class FacturaView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['facturas'] = Factura.objects.all()
         return context
+
+class CreateFacturaView(CreateView):
+    model = Factura
+    fields = [
+        'pago',
+        'ticket',
+    ]
+    template_name = 'factura_create.html'
+    success_url = '/factura'
+
+class UpdateFacturaView(UpdateView):
+    model = Factura
+    fields = [
+        'pago',
+        'ticket',
+    ]
+    template_name = 'factura_update.html'
+    success_url = '/factura'
+
+class DeleteFacturaView(DeleteView):
+    model = Factura
+    template_name = 'factura_delete.html'
+    success_url = '/factura'   
