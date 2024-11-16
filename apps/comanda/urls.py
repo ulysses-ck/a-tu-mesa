abedd99bdc40c9030cfc430bb78cc1575ead65fd
Ulises <ulisesapaza.280@gmail.com>
2024-11-16 20:14:28 -0300

    fix: change resource to single instead of plura

--- apps/comanda/urls.py
+++ apps/comanda/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comandas', views.ComandasView.as_view(), name='home'),
-    path('comandas/create/', views.CreateComandaView.as_view(), name='create_comanda'),
-    path('comandas/update/<int:pk>/', views.UpdateComandaView.as_view(), name='update_comanda'),
-    path('comandas/delete/<int:pk>/', views.DeleteComandaView.as_view(), name='delete_comanda'),
+    path('comanda', views.ComandasView.as_view(), name='home'),
+    path('comanda/create/', views.CreateComandaView.as_view(), name='create_comanda'),
+    path('comanda/update/<int:pk>/', views.UpdateComandaView.as_view(), name='update_comanda'),
+    path('comanda/delete/<int:pk>/', views.DeleteComandaView.as_view(), name='delete_comanda'),
 ]
