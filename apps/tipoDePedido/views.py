d174c900f899767c7ade35f7194e94e46b2e418d
Ulises <ulisesapaza.280@gmail.com>
2024-11-16 23:45:18 -0300

    feat: crud tipo pedido

--- apps/tipoDePedido/views.py
+++ apps/tipoDePedido/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import TipoDePedido
+
+class TipoDePedidoView(TemplateView):
+    name = 'TipoDePedido'
+    template_name = 'tipo_pedido_read.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tipoDePedidos'] = TipoDePedido.objects.all()
+        return context
+
+class CreateTipoDePedidoView(CreateView):
+    model = TipoDePedido
+    fields = ['nombre']
+    template_name = 'tipo_pedido_create.html'
+    success_url = '/tipoDePedido'
+
+class UpdateTipoDePedidoView(UpdateView):
+    model = TipoDePedido
+    fields = ['nombre']
+    template_name = 'tipo_pedido_update.html'
+    success_url = '/tipoDePedido'
+
+class DeleteTipoDePedidoView(DeleteView):
+    model = TipoDePedido
+    template_name = 'tipo_pedido_delete.html'
+    success_url = '/tipoDePedido'
