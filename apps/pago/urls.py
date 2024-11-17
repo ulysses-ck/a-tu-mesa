25145cb9f3d09acb5040c488d31308b83c4aba6c
Ulises <ulisesapaza.280@gmail.com>
2024-11-17 00:44:13 -0300

    feat: create crud for forma pago

--- apps/pago/urls.py
+++ apps/pago/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # pago
     path('pago', views.PagoView.as_view(), name='pago'),
     path('pago/create/', views.CreatePagoView.as_view(), name='create_pago'),
     path('pago/update/<int:pk>/', views.UpdatePagoView.as_view(), name='update_pago'),
     path('pago/delete/<int:pk>/', views.DeletePagoView.as_view(), name='delete_pago'),
+
+    # forma pago
+    path('forma_pago', views.FormaPagoView.as_view(), name='forma_pago'),
+    path('forma_pago/create/', views.CreateFormaPagoView.as_view(), name='create_forma_pago'),
+    path('forma_pago/update/<int:pk>/', views.UpdateFormaPagoView.as_view(), name='update_forma_pago'),
+    path('forma_pago/delete/<int:pk>/', views.DeleteFormaPagoView.as_view(), name='delete_forma_pago'),
 ]
--- apps/pago/views.py
+++ apps/pago/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
-from .models import Pago
+from .models import Pago, FormaDePago
 
+# pago
 class PagoView(TemplateView):
     name = 'Pago'
     template_name = 'pago_read.html'
@@ -35,3 +36,36 @@ class DeletePagoView(DeleteView):
     model = Pago
     template_name = 'pago_delete.html'
     success_url = '/pago'   
+
+
+# forma pago
+
+class FormaPagoView(TemplateView):
+    name = 'FormaPago'
+    template_name = 'forma_pago_read.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['formas_pagos'] = FormaDePago.objects.all()
+        return context
+    
+class CreateFormaPagoView(CreateView):
+    model = FormaDePago
+    fields = [
+        'nombre',
+    ]
+    template_name = 'forma_pago_create.html'
+    success_url = '/forma_pago'
+
+class UpdateFormaPagoView(UpdateView):
+    model = FormaDePago
+    fields = [
+        'nombre',
+    ]
+    template_name = 'forma_pago_update.html'
+    success_url = '/forma_pago'
+
+class DeleteFormaPagoView(DeleteView):
+    model = FormaDePago
+    template_name = 'forma_pago_delete.html'
+    success_url = '/forma_pago'
