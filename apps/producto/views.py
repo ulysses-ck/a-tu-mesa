d1d9ca09e2674410f9f83e454261704aaac46ed2
FacBar <facundobartolom3@gmail.com>
2024-11-17 18:32:07 -0300

    filtrado de productos bebida y comida

--- apps/producto/views.py
+++ apps/producto/views.py
@@ -1,13 +1,27 @@
 from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from .models import Producto
+from apps.tipoProducto.models import TipoProducto
 
 class ProductoView(TemplateView):
     name = 'producto'
     template_name = 'menu.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['productos'] = Producto.objects.all()
+        
+        # Obtener los tipos seleccionados desde los parámetros de la URL
+        tipos_seleccionados = self.request.GET.getlist('tipo')
+        
+        # Filtrar los productos según los tipos seleccionados
+        if tipos_seleccionados:
+            context['productos'] = Producto.objects.filter(tipo__nombre__in=tipos_seleccionados)
+        else:
+            context['productos'] = Producto.objects.all()
+        
+        # Obtener todos los tipos de productos para el formulario de filtro
+        context['tipos'] = TipoProducto.objects.all()
+        context['tipos_seleccionados'] = tipos_seleccionados
+        
         return context
     
 class ProductoAdminView(TemplateView):
