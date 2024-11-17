e43a61987f94d31d204b1d2ed60d993bc5ee877a
Ulises <ulisesapaza.280@gmail.com>
2024-11-16 22:41:27 -0300

    feat: crud for ticket

--- apps/ticket/urls.py
+++ apps/ticket/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('ticket', views.TicketView.as_view(), name='ticket'),
+    path('ticket/create/', views.CreateTicketView.as_view(), name='create_ticket'),
+    path('ticket/update/<int:pk>/', views.UpdateTicketView.as_view(), name='update_ticket'),    
+    path('ticket/delete/<int:pk>/', views.DeleteTicketView.as_view(), name='delete_ticket'),
 ]
--- apps/ticket/views.py
+++ apps/ticket/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView
 from .models import Ticket
 
 class TicketView(TemplateView):
@@ -7,5 +7,30 @@ class TicketView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['Ticket'] = Ticket.objects.all()
+        context['tickets'] = Ticket.objects.all()
         return context
+
+class CreateTicketView(CreateView):
+    model = Ticket
+    fields = [
+        'promocion',
+        'fecha',
+        'notas',
+    ]
+    template_name = 'ticket_create.html'
+    success_url = '/ticket'
+
+class UpdateTicketView(UpdateView):
+    model = Ticket
+    fields = [
+        'promocion',
+        'fecha',
+        'notas',
+    ]
+    template_name = 'ticket_update.html'
+    success_url = '/ticket'
+
+class DeleteTicketView(DeleteView):
+    model = Ticket
+    template_name = 'ticket_delete.html'
+    success_url = '/ticket'   
