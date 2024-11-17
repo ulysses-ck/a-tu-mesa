39ddfddc73d90133b58ef63aae4f34289271de67
Ulises <ulisesapaza.280@gmail.com>
2024-11-16 23:01:55 -0300

    fix: name url view

--- apps/comanda/urls.py
+++ apps/comanda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comanda', views.ComandasView.as_view(), name='home'),
+    path('comanda', views.ComandasView.as_view(), name='comanda'),
     path('comanda/create/', views.CreateComandaView.as_view(), name='create_comanda'),
     path('comanda/update/<int:pk>/', views.UpdateComandaView.as_view(), name='update_comanda'),
     path('comanda/delete/<int:pk>/', views.DeleteComandaView.as_view(), name='delete_comanda'),
