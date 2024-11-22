cf6f4de97203a297b2a2c0e4aec625f4b2db4100
Ulises <ulisesapaza.280@gmail.com>
2024-11-22 18:41:32 -0300

    feat: view for cocina

--- apps/comanda/urls.py
+++ apps/comanda/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('comanda/create/', views.CreateComandaView.as_view(), name='create_comanda'),
     path('comanda/update/<int:pk>/', views.UpdateComandaView.as_view(), name='update_comanda'),
     path('comanda/delete/<int:pk>/', views.DeleteComandaView.as_view(), name='delete_comanda'),
+
+    path('cocina/', views.CocinaView.as_view(), name='cocina'),
 ]
