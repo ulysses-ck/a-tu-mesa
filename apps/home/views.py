fc805ddca6ff7456ef26ab4708bada60c75a8c4c
Ulises <ulisesapaza.280@gmail.com>
2024-11-22 18:10:13 -0300

    fix: change absolute to relative values
    this in post to fix the issue with /media/ and cant use abolute routes

--- apps/home/views.py
+++ apps/home/views.py
@@ -27,10 +27,10 @@ class HomeView(TemplateView):
         home_instance, created = Home.objects.get_or_create(
             id=1,
             defaults={
-                "banner": "https://i.ibb.co/wWdxVVs/ilustracion-delicioso-shoyu-ramen-vista-arriba-fondo-blanco-435599-30529.jpg",
-                "botonMenu": "https://cdn.pixabay.com/photo/2017/11/07/07/06/menu-2925825_1280.png",
-                "botonReservation": "https://w7.pngwing.com/pngs/629/948/png-transparent-red-round-book-now-button-book-now-buttons-thumbnail.png",
-                "botonSeguimientoDePedido": "https://as2.ftcdn.net/v2/jpg/00/16/27/39/1000_F_16273947_3TlCeyAVBlJuphSPEDmllmLUeyAAQezK.jpg",
+                "banner": "home/example.jpg",
+                "botonMenu": "home/example.jpg",
+                "botonReservation": "home/example.jpg",
+                "botonSeguimientoDePedido": "home/example.jpg",
             },
         )
         if home_instance:
@@ -63,14 +63,10 @@ class HomeEditView(LoginRequidMixinWithLoginURL, TemplateView):
             home = Home.objects.create()
 
         # Actualizar usando los nombres correctos del modelo
-        home.banner = request.POST.get("banner", "")
-        home.botonMenu = request.POST.get("botonmenu", "")  # Notar la M mayúscula
-        home.botonReservation = request.POST.get(
-            "botonreservacion", ""
-        )  # Notar la R mayúscula
-        home.botonSeguimientoDePedido = request.POST.get(
-            "botonseguimientodepedido", ""
-        )  # Notar las mayúsculas
+        home.banner = request.FILES.get("banner")
+        home.botonMenu = request.FILES.get("botonmenu")  # Notar la M mayúscula
+        home.botonReservation = request.FILES.get("botonreservacion")  # Notar la R mayúscula
+        home.botonSeguimientoDePedido = request.FILES.get("botonseguimientodepedido")  # Notar las mayúsculas
 
         home.save()
         messages.success(request, "Contenido actualizado exitosamente")
@@ -87,10 +83,10 @@ class HomeAbout(TemplateView):
         home_instance, created = Home.objects.get_or_create(
             id=1,
             defaults={
-                "banner": "https://i.ibb.co/wWdxVVs/ilustracion-delicioso-shoyu-ramen-vista-arriba-fondo-blanco-435599-30529.jpg",
-                "botonMenu": "https://cdn.pixabay.com/photo/2017/11/07/07/06/menu-2925825_1280.png",
-                "botonReservation": "https://w7.pngwing.com/pngs/629/948/png-transparent-red-round-book-now-button-book-now-buttons-thumbnail.png",
-                "botonSeguimientoDePedido": "https://as2.ftcdn.net/v2/jpg/00/16/27/39/1000_F_16273947_3TlCeyAVBlJuphSPEDmllmLUeyAAQezK.jpg",
+                "banner": "home/example.jpg",
+                "botonMenu": "home/example.jpg",
+                "botonReservation": "home/example.jpg",
+                "botonSeguimientoDePedido": "home/example.jpg",
             },
         )
         if home_instance:
