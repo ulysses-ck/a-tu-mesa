a1456301a7de94ff5ef9db082d8cd821b82dc590
Ulises <ulisesapaza.280@gmail.com>
2024-11-22 18:09:11 -0300

    feat: change from text to imagefield

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Home(models.Model):
-    banner = models.CharField(max_length=300)
-    botonMenu = models.CharField(max_length=300)
-    botonReservation = models.CharField(max_length=300)
-    botonSeguimientoDePedido = models.CharField(max_length=300)
+    banner = models.ImageField(upload_to="home/")
+    botonMenu = models.ImageField(upload_to="home/")
+    botonReservation = models.ImageField(upload_to="home/")
+    botonSeguimientoDePedido = models.ImageField(upload_to="home/")
     
